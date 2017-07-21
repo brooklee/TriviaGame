258d686f41f709a40baeb4660f03621674fd0f46
brooklee1 <brooklee.wilson@gmail.com>
2017-07-20 20:33:58 -0600

    FIxed Populate Loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,109 +2,49 @@ $(function() {
 //-------------Global Var-----------
 
 //lets store our trivia questions in an object
-  var trivia = [
-  // question 1
-    {
-        question: "01. What is CSS?",
-        answers: ["Casscading Style Sheets", "Carrot steamed soup", "Corruoted style sheets", "Casscading stairs sheets"],
-        correctAnswer: 0
-    },
-    // question 2
-    {
-        question: "02. Q2?",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: 1
-    },
-    // question 3
-    {
-        question: "03. Q3?",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: 3
-    },
-    // question 4
-    {
-        question: "04. Q4?",
-        answers: ["1", "2", "3", "4"],
-        correctAnswer: 3
-    }
-];
-  
-    var timer = 10;
+var timer = 10;
     var timerId;
 
    var selections = [];//Holds Selections
     var score = $('#score');
     var counter = 0;
 
+  var trivia = [{
+    question: "01. What is CSS?",
+    answers: ["Casscading Style Sheets", "Carrot steamed soup", "Corruoted style sheets", "Casscading stairs sheets"],
+    correctAnswer: 0
+  },{
+    question: "02. Q2?",
+    answers: ["1", "2", "3", "4"],
+    correctAnswer: 1
+  },{
+    question: "03. Q3?",
+    answers: ["1", "2", "3", "4"],
+    correctAnswer: 3
+  },{
+    question: "04. Q4?",
+    answers: ["1", "2", "3", "4"],
+    correctAnswer: 3
+  }];
+  
+    
+
 //----------------Create trivia questions in Div---------------
 //start Button  populate
-  //Q1
+
   $("#startButton").on('click', function populate() {
-var testDiv = document.createElement("div");
-for (var i = 0; i < trivia.length; i++) {
-  testDiv.innerHTML = '<h3>' + trivia[0].question + '</h3>'+ '<div> <form> <p> <input type="radio" name ="answer0" value="right">'
-       + trivia[0].answers[0] +'</p>'
-       + '<p><input type="radio" name ="answer1" value="2">' + trivia[0].answers[1] +'</p>'
-       + '<p><input type="radio" name ="answer2" value="3">' +trivia[0].answers[2] +'</p>'
-       + '<p><input type="radio" name ="answer3" value="4">' +trivia[0].answers[3] +'</p>'
-       + '<p></form> </div>';
-  var questionsDiv = document.getElementById('questions');
-  questionsDiv.appendChild(testDiv);
-  }
-});
-
-  //Q2
-  $("#startButton").on('click', function populate() {
-var testDiv = document.createElement("div");
-for (var i = 0; i < trivia.length; i++) {
-  testDiv.innerHTML = '<h3>' + trivia[1].question + '</h3>'+ '<div> <form> <p> <input type="radio" name ="answer" value="1">'
-       + trivia[1].answers[0] +'</p>'
-       + '<p><input type="radio" name ="answer" value="2">' + trivia[1].answers[1] +'</p>'
-       + '<p><input type="radio" name ="answer" value="3">' +trivia[1].answers[2] +'</p>'
-       + '<p><input type="radio" name ="answer" value="4">' +trivia[1].answers[3] +'</p>'
-       + '<p></form> </div>';
-  var questionsDiv = document.getElementById('questions');
-  questionsDiv.appendChild(testDiv);
-  }
-});
-
-  //Q3
-  $("#startButton").on('click', function populate() {
-var testDiv = document.createElement("div");
-for (var i = 0; i < trivia.length; i++) {
-  testDiv.innerHTML = '<h3>' + trivia[2].question + '</h3>'+ '<div> <form> <p> <input type="radio" name ="answer" value="1">'
-       + trivia[2].answers[0] +'</p>'
-       + '<p><input type="radio" name ="answer" value="2">' + trivia[2].answers[1] +'</p>'
-       + '<p><input type="radio" name ="answer" value="3">' +trivia[2].answers[2] +'</p>'
-       + '<p><input type="radio" name ="answer" value="4">' +trivia[2].answers[3] +'</p>'
-       + '<p></form> </div>';
-  var questionsDiv = document.getElementById('questions');
-  questionsDiv.appendChild(testDiv);
-  }
-});
-
-  //Q4
-  $("#startButton").on('click', function populate() {
-var testDiv = document.createElement("div");
-for (var i = 0; i < trivia.length; i++) {
-  testDiv.innerHTML = '<h3>' + trivia[3].question + '</h3>'+ '<div> <form> <p> <input type="radio" name ="answer" value="1">'
-       + trivia[3].answers[0] +'</p>'
-       + '<p><input type="radio" name ="answer" value="2">' + trivia[3].answers[1] +'</p>'
-       + '<p><input type="radio" name ="answer" value="3">' +trivia[3].answers[2] +'</p>'
-       + '<p><input type="radio" name ="answer" value="4">' +trivia[3].answers[3] +'</p>'
-       + '<p></form> </div>';
-  var questionsDiv = document.getElementById('questions');
-  questionsDiv.appendChild(testDiv);
-  }
-});
-
- 
-  
-
-//add radio buttons
-  //Come back and loop the populate
-
-  
+    var testDiv = document.createElement("div");
+    for (var i = 0; i < trivia.length; i++) {
+      testDiv.innerHTML += '<h3>' + trivia[i].question + '</h3>' + '<div><form><p><input type="radio" name="answer" value="right">' +
+      trivia[i].answers[0] + '</p>' +
+        '<p><input type="radio" name ="answer" value="2">' + trivia[i].answers[1] + '</p>' +
+        '<p><input type="radio" name ="answer" value="3">' + trivia[i].answers[2] + '</p>' +
+        '<p><input type="radio" name ="answer" value="4">' + trivia[i].answers[3] + '</p>' +
+        '<p></form> </div>';
+    }
+    var questionsDiv = document.getElementById('questions');
+    questionsDiv.appendChild(testDiv);
+  });
 
 
   //------------------Start Game-------------------
@@ -188,4 +128,5 @@ for (var i = 0; i < trivia.length; i++) {
 //-----------------Finish Button------------------------
 // when finish button is click skip timer to 0
 });
+
 
